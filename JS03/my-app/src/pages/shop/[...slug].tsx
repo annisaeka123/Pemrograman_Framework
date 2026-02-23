c711d70c558973f0f582a5a370bcb69873c68067
annisa eka <annisaekapuspitaaa@gmail.com>
2026-02-23 07:12:59 +0700

    JS03: Langkah 3 (Pengujian Catch-All Route)

--- JS03/my-app/src/pages/shop/[...slug].tsx
+++ JS03/my-app/src/pages/shop/[...slug].tsx
@@ -8,8 +8,10 @@ const halamanToko = () => {
     <div>
       <h1>Halaman Shop</h1>
       <p>Toko: {`${query.slug && query.slug[0]+"-"+query.slug[1]}`}</p> {/* menggunakan backtick (`) bukan petik satu (') */}
-      {/* query.slug[0] untuk mengambil bagian pertama dari slug */}
-      {/* query.slug[1] untuk mengambil bagian kedua dari slug */}
+
+      {/* <p>
+        Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}
+      </p> */}
     </div>
   )
 }
