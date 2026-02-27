23b7e950430f379f3e1b13f5d5b39390d7115b54
annisa eka <annisaekapuspitaaa@gmail.com>
2026-02-28 01:29:49 +0700

    JS06: 3. Pengaturan Title per Halaman

--- JOBSHEET-06/my-app/src/pages/index.tsx
+++ JOBSHEET-06/my-app/src/pages/index.tsx
@@ -10,9 +10,12 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <h1>Praktikum Next.js Pages Router</h1> <br />
-      <p>Annisa D4 Pengembangan Web</p>
-      <Link href="/about"><button>About Annisa</button></Link>
+      <head>
+        <title>Praktikum Next.js Pages Router</title>
+      </head>
+        <h1>Praktikum Next.js Pages Router</h1> <br />
+        <p>Annisa D4 Pengembangan Web</p>
+        <Link href="/about"><button>About Annisa</button></Link>
     </div>
   )
 }
