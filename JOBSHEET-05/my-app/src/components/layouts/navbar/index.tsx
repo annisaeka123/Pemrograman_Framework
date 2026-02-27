25d20169273f6d1245b5f84999e89f6303e9343b
annisa eka <annisaekapuspitaaa@gmail.com>
2026-02-27 21:47:21 +0700

    JS05: Langkah 1 (Global CSS) & Langkah 2 (CSS Module (Local Scope))

--- JOBSHEET-05/my-app/src/components/layouts/navbar/index.tsx
+++ JOBSHEET-05/my-app/src/components/layouts/navbar/index.tsx
@@ -1,6 +1,7 @@
+import styles from './navbar.module.css'
 const Navbar = () => {
     return (
-        <div className="navbar">
+        <div className={styles.navbar}>
             <div>navbar Component</div>
         </div>
     )
