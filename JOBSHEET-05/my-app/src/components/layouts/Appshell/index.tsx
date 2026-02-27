67243fa3425fa971205ff640c276f83278e5807c
annisa eka <annisaekapuspitaaa@gmail.com>
2026-02-27 22:11:15 +0700

    JS05: 4. Conditional Rendering Navbar (Tanpa Navbar di Login)

--- JOBSHEET-05/my-app/src/components/layouts/Appshell/index.tsx
+++ JOBSHEET-05/my-app/src/components/layouts/Appshell/index.tsx
@@ -1,20 +1,24 @@
-import Navbar from "../navbar";
-import Footer from "../footer";
+import { useRouter } from "next/router"
+import Navbar from "../navbar"
+import Footer from "../footer"
+
+const disableNavbar = ['/auth/login', '/auth/register']
 
 type AppShellProps = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 const AppShell = (props: AppShellProps) => {
-    const { children } = props
+  const { children } = props
+  const { pathname } = useRouter()
 
-    return (
-        <main>
-            <Navbar />
-            {children}
-            <Footer />
-        </main>
-    )
+  return (
+    <main>
+      {!disableNavbar.includes(pathname) && <Navbar />}
+      {children}
+      <Footer />
+    </main>
+  )
 }
 
 export default AppShell
