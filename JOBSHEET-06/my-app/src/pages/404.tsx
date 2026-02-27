978302e9895b64b4428fb5a9b300237d67456a24
annisa eka <annisaekapuspitaaa@gmail.com>
2026-02-28 02:16:35 +0700

    JS06: Tugas Praktikum (Tugas 1, Tugas 2, Tugas 3)

--- JOBSHEET-06/my-app/src/pages/404.tsx
+++ JOBSHEET-06/my-app/src/pages/404.tsx
@@ -1,11 +1,33 @@
 import styles from "@/styles/404.module.scss"
+import Link from "next/dist/client/link"
 
 const Customer404 = () => {
   return (
     <div className={styles.error}>
-        <img src="/page-not-found.png" alt="404" className={styles.error} />
-      <h1>404 - Halaman Tidak Ditemukan</h1>
-      <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
+      <head>
+        <title>404 - Halaman Tidak Ditemukan</title>
+      </head>
+       <div className={styles.error}>
+        <img
+          src="/page-not-found.png"
+          alt="Halaman tidak ditemukan"
+          className={styles.error_image}
+        />
+
+        <h1 className={styles.title}>404</h1>
+
+        <h2 className={styles.subtitle}>
+          Halaman Tidak Ditemukan
+        </h2>
+
+        <p className={styles.description}>
+          Maaf, halaman yang Anda cari tidak ditemukan atau sudah dipindahkan.
+        </p>
+
+        <Link href="/" className={styles.button}>
+          Kembali ke Home
+        </Link>
+      </div>
     </div>
   )
 }
