e63590bec199fadc5d5fc07cd63cbb9b9c463ae6
annisa eka <annisaekapuspitaaa@gmail.com>
2026-02-28 02:05:43 +0700

    JS06: 6. Menampilkan Gambar dari Folder Public

--- JOBSHEET-06/my-app/src/pages/404.tsx
+++ JOBSHEET-06/my-app/src/pages/404.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/404.module.scss"
 const Customer404 = () => {
   return (
     <div className={styles.error}>
+        <img src="/page-not-found.png" alt="404" className={styles.error} />
       <h1>404 - Halaman Tidak Ditemukan</h1>
       <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
     </div>
