6e59c1d6af6fc3ae55105369f7fa1f83eec9477d
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-03 21:43:01 +0700

    JS07: Integrasi Firebase - Langkah 5 (Setup Firebase) dan Langkah 6 (Install Firebase)

---
+++ JOBSHEET-07/my-app/src/pages/utlis/db/firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCs4gQO8_3sH5qDIp8IYTctgQjkFeqbEyg",
+  authDomain: "framework-next-5db08.firebaseapp.com",
+  projectId: "framework-next-5db08",
+  storageBucket: "framework-next-5db08.firebasestorage.app",
+  messagingSenderId: "415028575135",
+  appId: "1:415028575135:web:cb3f8da44a4fb5b1f41baa"
+};
