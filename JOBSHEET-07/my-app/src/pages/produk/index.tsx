cc9e9655b55653a9801dea92bf25077f243afede
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-03 21:31:50 +0700

    JS07: Langkah 3 (Fetch Data API di Frontend)

--- JOBSHEET-07/my-app/src/pages/produk/index.tsx
+++ JOBSHEET-07/my-app/src/pages/produk/index.tsx
@@ -1,12 +1,40 @@
-import TampilanProduk from "../views/produk"
+import { useEffect, useState } from "react"
 
-const halamanProduk = () => {
+type ProductType = {
+  id: string
+  nama: string
+  harga: number
+  ukuran: string
+  warna: string
+}
+
+const kategori = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch("/api/produk")
+      .then((response) => response.json())
+      .then((responsedata) => {
+        setProducts(responsedata.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching produk:", error);
+      })
+  }, [])
 
   return (
-    <>
-    <TampilanProduk />
-    </>
+  <div>
+    <h1>Daftar Produk</h1>
+    {products.map((product:ProductType) => (
+    <div key={product.id}>
+      <h2>{product.nama}</h2>
+      <p>Harga: {product.harga}</p>
+      <p>Ukuran: {product.ukuran}</p>
+      <p>Warna: {product.warna}</p>
+    </div>
+    ))}
+  </div>
   )
 }
 
-export default halamanProduk
+export default kategori
