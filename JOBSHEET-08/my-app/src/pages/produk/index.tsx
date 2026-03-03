f7435a4dc5cc6a0b134904f21cc7700b93d81e87
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-04 01:52:02 +0700

    JS08: Bagian 4 (Implementasi SWR)

--- JOBSHEET-08/my-app/src/pages/produk/index.tsx
+++ JOBSHEET-08/my-app/src/pages/produk/index.tsx
@@ -1,27 +1,17 @@
 import { useEffect, useState } from "react"
 import TampilanProduk from "../views/produk"
+import useSWR from "swr"
+import fetcher from "../utils/swr/fetcher"
 
+// const fetcher = (url: string) => fetch(url).then((res) => res.json())
 const kategori = () => {
   const [products, setProducts] = useState([])
 
-  const fetchProducts = () => {
-    fetch("/api/produk")
-      .then((response) => response.json())
-      .then((responsedata) => {
-        setProducts(responsedata.data)
-      })
-      .catch((error) => {
-        console.error("Error fetching produk:", error)
-      })
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher)
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data.data} />
     </div>
   )
 }
