9d07728cbb1bf5c4b20cfae00ca4098856ad33a2
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-04 09:08:04 +0700

    JS08: update untuk /produk/server

--- JOBSHEET-08/my-app/src/pages/produk/[id].tsx
+++ JOBSHEET-08/my-app/src/pages/produk/[id].tsx
@@ -1,13 +1,21 @@
 import { useRouter } from "next/router"
+import TampilanProduk from "../views/produk"
+import useSWR from "swr"
+import fetcher from "../utils/swr/fetcher"
 
 const HalamanProduk = () => {
     // const Router = useRouter()
     // console.log(Router)
     const { query } = useRouter()
+    const { data, error, isLoading } = useSWR(
+        query.id ? `/api/produk?kategori=${query.id}` : null, 
+        fetcher
+    )
+
     return (
         <div>
-            <h1>Halaman Produk</h1>
-            <p>Produk: {query.id}</p>
+            <h1>Halaman Produk {query.id}</h1>
+            <TampilanProduk products={isLoading ? [] : data?.data || []} />
         </div>
     )
 }
