5a0d27cc14679b632cd69565dfaaf6319cc1da1c
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-07 21:08:35 +0700

    JS11: Bagian 2 (Implementasi CSR (Client Rendering))

---
+++ JOBSHEET-11/my-app/src/pages/produk/[produk].tsx
@@ -0,0 +1,37 @@
+// import fetcher from "@/utils/swr/fetcher"
+// import { useRouter } from "next/router"
+// import useSWR from "swr";
+
+// const HalamanProduk = () => {
+//   // const Router = useRouter();
+//   // console.log(Router);
+//   const { query } = useRouter();
+//   const {data,error,isLoading} = useSWR(`/api/products/${query.produk}`,fetcher);
+//   return (
+//   <div>
+//     <h1>Halaman Produk</h1>
+//     <p>Produk: {query.produk}</p>
+//   </div>
+//   )
+// }
+
+// export default HalamanProduk
+
+import fetcher from "@/utils/swr/fetcher"
+import { useRouter } from "next/router"
+import useSWR from "swr"
+import DetailProduk from "../../views/DetailProduct"
+
+const HalamanProduk = () => {
+  // const Router = useRouter()
+  // console.log(Router)
+  const { query } = useRouter()
+  const{data,error,isLoading} = useSWR(`/api/products/${query.produk}`,fetcher)
+  return (
+  <div>
+    <DetailProduk products={isLoading ? [] : data.data} />
+  </div>
+  )
+}
+
+export default HalamanProduk
