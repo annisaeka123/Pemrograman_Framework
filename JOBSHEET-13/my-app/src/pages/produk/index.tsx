fdd132c1defb883e2a2e401ef21dc07b7b589b2d
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-12 18:52:34 +0700

    JS13: Bagian 1 (Membuat Middleware)

--- JOBSHEET-13/my-app/src/pages/produk/index.tsx
+++ JOBSHEET-13/my-app/src/pages/produk/index.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react"
 import TampilanProduk from "../../views/produk"
 import useSWR from "swr"
 import fetcher from "../../utils/swr/fetcher"
+import { useRouter } from "next/dist/client/components/navigation"
 
 // const fetcher = (url: string) => fetch(url).then((res) => res.json())
 const kategori = () => {
+  const { push } = useRouter()
   const [products, setProducts] = useState([])
 
   const { data, error, isLoading } = useSWR("/api/produk", fetcher)
