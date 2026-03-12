07cd916358b27daf789066c54ecfbe448b78d1b0
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-12 21:56:34 +0700

    JS14: Proteksi Halaman Profile (Buat Halaman Profile)

--- JOBSHEET-14/my-app/src/pages/profile/index.tsx
+++ JOBSHEET-14/my-app/src/pages/profile/index.tsx
@@ -1,20 +1,12 @@
-import Link from "next/link"
+import { useSession } from "next-auth/react"
+// import Link from "next/link"
 
 const ProfilePage = () => {
+  const {data}:any = useSession()
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Halaman Profile</h1>
-
-      <div style={{ marginTop: "20px" }}>
-        <p><strong>Nama:</strong> Annisa Eka Puspita</p>
-        <p><strong>Email:</strong> annisa@gmail.com</p>
-      </div>
-
-      <div style={{ marginTop: "20px" }}>
-        <Link href="/profile/edit">
-          <button>Edit Profile</button>
-        </Link>
-      </div>
+    <div>
+      <h1>Halaman Profile</h1> <br />
+      <h1>Selamat Datang {data?.user?.fullname}</h1>
     </div>
   )
 }
