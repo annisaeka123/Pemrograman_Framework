f46785e0525a3b9b4ef0a51a508da3ae65cfb9ec
annisa eka <annisaekapuspitaaa@gmail.com>
2026-03-12 21:49:37 +0700

    JS14:  Menambahkan Data Tambahan (Full Name)

--- JOBSHEET-14/my-app/src/components/layouts/navbar/index.tsx
+++ JOBSHEET-14/my-app/src/components/layouts/navbar/index.tsx
@@ -1,18 +1,39 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
 import styles from './navbar.module.css'
-const Navbar = () => {
-    const { data } = useSession()
+import { signIn, signOut, useSession } from "next-auth/react"
 
-    return (
-        <div className={styles.navbar}>
-            <div className="big">navbar Component</div>
-            {data ? (
-                <button onClick={() => signOut()}>Sign Out</button>
-            ) : (
-                <button onClick={() => signIn()}>Sign In</button>
-            )}
-        </div>
-    )
+const Navbar = () => {
+  const {data}:any = useSession()
+
+return (
+  <div className={styles.navbar}>
+    <div className={styles.navbar__brand}>
+      MyApp
+    </div>
+
+    <div className={styles.navbar__right}>
+      {data ? (
+        <>
+          <div className={styles.navbar__user}>
+            Welcome, {data.user?.fullname}
+          </div>
+          <button
+            className={`${styles.navbar__button} ${styles["navbar_button--danger"]}`}
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button>
+        </>
+      ) : (
+        <button
+          className={`${styles.navbar__button} ${styles["navbar_button--primary"]}`}
+          onClick={() => signIn()}
+        >
+          Sign In
+        </button>
+      )}
+    </div>
+  </div>
+)
 }
 
 export default Navbar
