4b1d8479544261f167beb45132ea8e31bd33b77f
annisa eka <annisaekapuspitaaa@gmail.com>
2026-04-06 15:01:57 +0700

    JS18: Tugas Praktikum 3 ( script Google Analytics menggunakan next/script )

--- JOBSHEET-18/my-app/src/pages/_app.tsx
+++ JOBSHEET-18/my-app/src/pages/_app.tsx
@@ -1,15 +1,35 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import AppShell from "@/components/layouts/Appshell"
-import Navbar from "@/components/layouts/navbar"
 import { SessionProvider } from "next-auth/react"
+import Script from "next/script"
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
-    </SessionProvider>
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-VM4M076CNZ"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          console.log("GA Loaded ✅");
+
+          window.dataLayer = window.dataLayer || [];
+          window.gtag = function(){window.dataLayer.push(arguments);};
+
+          window.gtag('js', new Date());
+          window.gtag('config', 'G-VM4M076CNZ');
+        `}
+      </Script>
+
+      <SessionProvider session={session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
+    </>
   )
 }
