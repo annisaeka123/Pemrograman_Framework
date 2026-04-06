50753ad616879b0671a1a2127735d2fc587e12b8
annisa eka <annisaekapuspitaaa@gmail.com>
2026-04-06 11:57:30 +0700

    JS17: Tugas Praktikum 5 (Gunakan next/image untuk optimasi avatar)

--- JOBSHEET-17/my-app/src/components/layouts/navbar/index.tsx
+++ JOBSHEET-17/my-app/src/components/layouts/navbar/index.tsx
@@ -1,46 +1,49 @@
 import styles from './navbar.module.css'
 import { signIn, signOut, useSession } from "next-auth/react"
+import Image from "next/image"
 
 const Navbar = () => {
-  const {data}:any = useSession()
+  const { data }: any = useSession()
 
-return (
-  <div className={styles.navbar}>
-    <div className={styles.navbar__brand}>
-      MyApp
-    </div>
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.navbar__brand}>
+        MyApp
+      </div>
 
-    <div className={styles.navbar__right}>
-      {data ? (
-        <>
-          <div className={styles.navbar__user}>
-            Welcome, {data.user?.fullname}
-            {data.user?.image && (
-              <img
-                src={data.user.image}
-                alt={data.user.fullname}
+      <div className={styles.navbar__right}>
+        {data ? (
+          <>
+            <div className={styles.navbar__user}>
+              Welcome, {data.user?.fullname}
+
+              <Image
+                src={data.user?.image || "/default-avatar.png"}
+                alt={data.user?.fullname || "User Avatar"}
+                width={32}
+                height={32}
                 className={styles.navbar__user__image}
               />
-            )}
-          </div>
+            </div>
+
+            <button
+              className={`${styles.navbar__button} ${styles["navbar__button--danger"]}`}
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </button>
+          </>
+        ) : (
           <button
-            className={`${styles.navbar__button} ${styles["navbar__button--danger"]}`}
-            onClick={() => signOut()}
+            className={`${styles.navbar__button} ${styles["navbar__button--primary"]}`}
+            onClick={() => signIn()}
           >
-            Sign Out
+            Sign In
           </button>
-        </>
-      ) : (
-        <button
-          className={`${styles.navbar__button} ${styles["navbar_button--primary"]}`}
-          onClick={() => signIn()}
-        >
-          Sign In
-        </button>
-      )}
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default Navbar
--- JOBSHEET-17/my-app/src/pages/profile/index.tsx
+++ JOBSHEET-17/my-app/src/pages/profile/index.tsx
@@ -1,12 +1,36 @@
 import { useSession } from "next-auth/react"
-// import Link from "next/link"
+import Image from "next/image"
+import styles from "./profile.module.scss"
 
 const ProfilePage = () => {
-  const {data}:any = useSession()
+  const { data }: any = useSession()
+
   return (
-    <div>
-      <h1>Halaman Profile</h1> <br />
-      <h1>Selamat Datang {data?.user?.fullname}</h1>
+    <div className={styles.profile}>
+      <h1 className={styles.profile__title}>Halaman Profile</h1>
+
+      <h2 className={styles.profile__welcome}>
+        Selamat Datang {data?.user?.fullname}
+      </h2>
+
+      <div className={styles.profile__card}>
+        <div className={styles.profile__card__image}>
+          <Image
+            src={data?.user?.image || "/default-avatar.png"}
+            width={140}
+            height={140}
+            alt="profile"
+            priority
+          />
+        </div>
+        <div className={styles.profile__card__name}>
+          {data?.user?.fullname}
+        </div>
+
+        <div className={styles.profile__card__email}>
+          {data?.user?.email}
+        </div>
+      </div>
     </div>
   )
 }
